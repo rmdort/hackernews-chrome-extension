f2533b110e61a22da8d89be131ed9afaea228484
Vinay <rmdort@gmail.com>
2020-05-04 01:27:27 +0800

    Added logo

---
+++ src/components/HNLogo.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Box } from "@chakra-ui/core"
+
+const Logo = () => {
+  return (
+    <Box
+      color="white"
+      background="#FC6723"
+      width={30}
+      height={30}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      margin="auto"
+      mb={4}
+      fontSize={20}
+    >
+      Y
+    </Box>
+  )
+}
+
+export default Logo
