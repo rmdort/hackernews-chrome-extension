076ed63562d96be34f5af40dad31427f1b4b6845
Vinay <rmdort@gmail.com>
2020-05-04 01:27:17 +0800

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React, { Suspense, useState, useTransition, useRef, useEffect } from "react"
+import React, {
+  Suspense,
+  useState,
+  useTransition,
+  useRef,
+  useEffect,
+} from "react"
 import SearchBar from "./components/SearchBar"
 import Results from "./components/Results"
-import HNLogo from './components/HNLogo'
+import HNLogo from "./components/HNLogo"
 import { search, IPromiseWrapper, IResults } from "./api/hn"
 import { Box, Spinner, useColorMode, IconButton } from "@chakra-ui/core"
 
-const QUERY_KEY = 'query'
-const initialQuery = localStorage.getItem(QUERY_KEY) || ''
+const QUERY_KEY = "query"
+const initialQuery = localStorage.getItem(QUERY_KEY) || ""
 const App = () => {
   const [query, setQuery] = useState<string>(initialQuery)
-  const [resource, setResource] = useState<IPromiseWrapper<IResults>>(() => search<IResults>({ query }))
+  const [resource, setResource] = useState<IPromiseWrapper<IResults>>(() =>
+    search<IResults>({ query })
+  )
   const inputRef = useRef<HTMLInputElement | null>(null)
   const [startTransition, isPending] = useTransition({
     timeoutMs: 2000,
@@ -31,12 +39,12 @@ const App = () => {
     try {
       localStorage.setItem(QUERY_KEY, query)
     } catch (err) {
-      console.error('Error saving in localstorate', err)
+      console.error("Error saving in localstorate", err)
     }
-  }, [ query ])
+  }, [query])
 
   return (
-    <Box margin="auto" width={800} py={4}>      
+    <Box margin="auto" width={800} py={4}>
       <IconButton
         aria-label="Switch to dark mode"
         icon={colorMode === "dark" ? "sun" : "moon"}
